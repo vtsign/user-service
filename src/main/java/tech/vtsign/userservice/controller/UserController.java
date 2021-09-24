fff00edcbe17a780f7d858e4e11bacecd7247167
hdecode-tuanchau <tuanchau-student@hdexpertise.com>
2021-09-24 22:22:14 +0700

    update controller

--- src/main/java/tech/vtsign/userservice/controller/UserController.java
+++ src/main/java/tech/vtsign/userservice/controller/UserController.java
@@ -1,2 +1,18 @@
-package tech.vtsign.userservice;public class UserController {
+package tech.vtsign.userservice.controller;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(@RequestHeader("Authorization") String language){
+
+        return ResponseEntity.ok(language);
+    }
 }
